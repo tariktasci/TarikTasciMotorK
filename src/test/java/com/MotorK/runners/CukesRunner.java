b2bac68f8802c555a1c542ba163306d33432a2df
tariktasci <tsctarik@gmail.com>
2021-05-29 11:55:06 +0200

    runners files were created.

--- src/test/java/com/MotorK/runners/CukesRunner.java
+++ src/test/java/com/MotorK/runners/CukesRunner.java
@@ -2,9 +2,18 @@ package com.MotorK.runners;
 
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
+@CucumberOptions(
+        plugin ={"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
+        features ="src/test/resources/features",
+        glue = "com/MotorK/step_definitions",
+        dryRun = false,
+        tags = "@smoke"
+)
 public class CukesRunner {
 }
